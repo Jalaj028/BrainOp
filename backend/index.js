f11408383f5c2a317724a17aac3254003a832e47
Jalaj2004 <jalajbhatt2004@gmail.com>
2024-06-03 20:49:23 +0530

    modified cors

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,7 @@ app.use(bodyParser.json());
 
 app.use(cors(
     {
-        origin:["http://127.0.0.1:5173",],
+        origin:["http://127.0.0.1:5173","https://brain-op-jalaj.vercel.app"],
         methods:["GET", "POST"],
         credentials: true
     }
