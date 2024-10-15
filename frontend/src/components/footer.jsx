4cbb04ec8225427bee835c98de47c26f73ec6d94
Jalaj Bhatt <117558817+Jalaj028@users.noreply.github.com>
2024-10-15 15:38:06 +0530

    Update footer.jsx

--- frontend/src/components/footer.jsx
+++ frontend/src/components/footer.jsx
@@ -5,6 +5,7 @@ function Footer() {
         <div>
             <div className="bg-primary min-h-80 text-white flex justify-center items-center">
                 <h1 className="text-xl font-bold">BrainOp</h1>
+                <p>Created by: Aviral Joshi, Aakash Rawat, Deepak Palariya, Kartikey Singh, Jalaj Bhatt</p>
             </div>
         </div>
     )
